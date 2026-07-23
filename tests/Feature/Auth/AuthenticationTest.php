81fb607164c61b85f1eb487ceefc0349bff6adab
Fauzan Azmi <fauzan.azmi@suryaenergi.com>
2026-07-23 09:42:35 +0700

    fix(tests): update redirect assertions for subdomain relative root redirect

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -58,9 +58,9 @@ test('users can not authenticate with invalid password', function () {
 test('users can logout', function () {
     $user = User::factory()->create();
 
-    $response = $this->actingAs($user)->post(route('logout'));
+    $response = $this->actingAsStaff($user)->post(route('logout'));
 
-    $response->assertRedirect(route('home'));
+    $response->assertRedirect('/');
 
     $this->assertGuest();
 });
--- tests/Feature/Auth/VerificationNotificationTest.php
+++ tests/Feature/Auth/VerificationNotificationTest.php
@@ -16,7 +16,7 @@ test('sends verification notification', function () {
 
     $this->actingAsStaff($user)
         ->post(route('verification.send'))
-        ->assertRedirect(route('home'));
+        ->assertRedirect('/');
 
     Notification::assertSentTo($user, VerifyEmail::class);
 });
